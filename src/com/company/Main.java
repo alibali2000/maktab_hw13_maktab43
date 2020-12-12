9a72402afc71efe1043282aa2e5f1f517c5b6cd9
ali.rostamian <alibalii2000@gmail.com>
2020-12-12 12:31:55 +0330

    initialing git and adding project's existing files

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+    }
+}
